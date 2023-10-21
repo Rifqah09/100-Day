df4eec2fb4d724d2ed8a10f92bcd00d929f973b4
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-21 20:45:43 +0800

    Create Day-13.java

---
+++ Day-13.java
@@ -0,0 +1,13 @@
+package penugasan;
+public class Penugasan {
+    
+    public static void main(String[] args) {
+        int angka = 15;
+        
+        //operator penugasan 
+        angka += 5;
+        
+        System.out.println("angka setelah di tambah : " + angka);
+    }
+    
+}
