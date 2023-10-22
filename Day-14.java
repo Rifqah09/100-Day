fc454657770b3535d01716e9eb369c13e7d4850b
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-22 13:18:13 +0800

    Create Day-14.java

---
+++ Day-14.java
@@ -0,0 +1,12 @@
+package day14;
+  public class Day14 {
+    public static void main(String[] args) {
+        double pi, r, r;
+
+        r = 15.0;
+        pi = 3.14;
+
+        //proses
+        Luas = pi * r * r;
+
+        System.out.println("Luas Lingkaran = " + Luas);
