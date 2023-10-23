536ad216dcafa7aed972d992a6268af479ce4a50
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-23 20:23:33 +0800

    Create Day-15.java

---
+++ Day-15.java
@@ -0,0 +1,14 @@
+package tipecasting;
+public class TipeCasting {
+  public static void main(String[] args) {
+      int angka = 50;
+      String angka1 = Integer.toString(angka);
+      System.out.println(angka1);
+
+
+  }
+
+}
+  
+
+  
