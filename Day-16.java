cbe7361757b831a64a9dea316e62c3732a5ff321
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-24 20:20:18 +0800

    Create Day-16.java

---
+++ Day-16.java
@@ -0,0 +1,14 @@
+package PenugasanPerkalian1;
+
+public class PenugasanPerkalian1 {
+    
+    public static void main(String[] args) {
+        int angka1 = 6;
+        int angka2 = 8;
+        
+        //operator penugasan perkalian
+        int hasilPerkalian = angka1 * angka2;
+        System.out.println("Hasil Perkalian :" + hasilPerkalian);
+    }
+    
+}
