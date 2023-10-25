051771ca5b480d7149ef4fdb02d4160c6d425819
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-25 19:55:52 +0800

    Create Day-17.java

---
+++ Day-17.java
@@ -0,0 +1,11 @@
+package day17;
+public class Day17 {
+  public static void main(String[] args) {
+    int A = 7;
+    int B = 9;
+    System.out.println(A < B);
+    System.out.println( A != B);
+
+  }
+
+}
