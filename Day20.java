a8278e5dbd7f5c2b5d171ecbafe4af9016f06bb3
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-28 09:19:17 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,25 @@
+package day20;
+
+import java.util.Scanner;
+public class Day20 {
+  public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println.("Masukkan Gaji Bulanan : ");
+      double gaji = input.nextDouble();
+      System.out.println("Gaji :" +. gaji);
+
+      if (gaji >= 3000000 && gaji < 5000000) {
+        System.out.println("Pajak 5%);
+        Syste.out.println("Gaji : " + (gaji-(gaji * 0.05)));
+
+      }else if (gaji >= 5000000) {
+        System.out.println("Pajak 10%");
+        System.out.println("Gaji : " + ( gaji-(gaji * 0.1)));
+
+      }else{
+        System.out.println("Tidak kena pajak");
+
+      }
+  }
+
+}
