581b3265a9589c43f4e7d0ae047069391efdc4d3
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-29 20:44:08 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,17 @@
+package day21;
+public class Day21 {
+  public static void main(String[] args) {
+    System.out.println("Target push up : ");
+    int targetPushup = 50;
+    int pushUp = 0;
+
+    while (pushUp < targetPushup) {
+      pushUp++;
+      System.out.println("Anda telah melakukan : " + pushUp + "Push-up");
+      System.out.println("Selamat! Anda telah mencapai target" + targetPushup + " Push-up");
+
+    }
+
+  }
+
+}
