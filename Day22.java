934f8d2494100bbc28e5e79f48e1c922b7c7f821
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-30 20:31:36 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,18 @@
+package day.pkg22;
+public class Day22 {
+  public static void main(String[] args) {
+    for (int i = 1; <= 2; i++) {
+      System.out.println("Bagian a : " + i);
+
+    for (int j = 1; <= 5; j++) {
+      System.out.println("Bagian b : " + j);
+
+
+
+    }
+
+    }
+
+  }
+
+}
