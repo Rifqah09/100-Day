9173bd8865e3106f0f37e4adebe6797d1711018e
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-10-31 20:46:00 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,23 @@
+package day23;
+import java.util.Scanner;
+
+public class Day23 {
+  public static void main(String{} args) { 
+      Scanner input = new Scanner (System.in);
+      System.out.println("Masukkan umur : ");
+      int umur = input.nextInt();
+      int unur1 = 18;
+ 
+      String status = ( umur >= 18) ? "Dewasa" : "Remaja";
+      System.out.println("Status : + status);
+
+      String jawaban = input.nextLine();
+
+
+
+  }
+
+
+}
+
+
