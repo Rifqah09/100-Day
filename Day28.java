adb68c7a842d16f01a4817bcc9a681226bf74168
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-05 18:45:17 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,15 @@
+package Day28;
+
+public class Day28 {
+    
+    public static void main(String[] args) {
+        int total = 0;
+        for (int a = 1; a <=2; a++) {
+            for (int b = 1; b <=2; b++) {
+                total = total + a;
+            }
+        }
+        System.out.println(total);
+    }
+    
+}
