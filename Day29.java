bab998587e2d939ad9c5901564a9957b7c52c879
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-06 14:13:41 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,16 @@
+package Day29;
+
+public class Day29 {
+    
+    public static void main(String[] args) {
+        int tinggibadan = 154;
+        
+        if(tinggibadan > 165){
+            System.out.println("Kamu lebih tinggi dari teman mu ");
+        }else{
+            System.out.println("Kamu lebih pendek dari teman mu");
+        }
+        
+    }
+    
+}
