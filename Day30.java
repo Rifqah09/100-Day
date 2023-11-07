849cb1174a31b36dee458494afba710cc02e4017
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-07 22:08:26 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,18 @@
+package tesa;
+
+public class tesa {
+    
+    public static void main(String[] args) {
+         int sisi = 10;
+                for (int i = 1; i <= sisi; i++) {
+            for (int j = 1; j <= i ; j++) {
+                {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+                }
+        
+    }
+    
+}
