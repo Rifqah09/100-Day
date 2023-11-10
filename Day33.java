24f8ae356354c2f09662b0fea9ffc000a0ef1ca9
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-10 15:56:06 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,12 @@
+package Day33;
+
+public class Day33 {
+    
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+        boolean c = (a>4) && (b>8);
+        System.out.println(c);
+    }
+    
+}
