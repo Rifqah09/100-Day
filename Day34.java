c1c6d2e4086e4d63099cda69a38506d9bce47a1a
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-11 19:09:39 +0800

    Create Day34.java

---
+++ Day34.java
@@ -0,0 +1,15 @@
+package day34;
+
+public class Day34 {
+    
+    public static void main(String[] args) {
+        String[] buah = {"Melon", "Samangka", "Rambutan", "Durian", "Mangga"};
+        
+        int a = 9 / 3;
+        for (int i = 0; i < a; i++) {
+            System.out.println(buah[i]);
+        }
+        
+    }
+    
+}
