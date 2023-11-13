c86c2e99841e4c28667bb973bd72aa6992d1059a
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-13 20:04:58 +0800

    Create Day36.java

---
+++ Day36.java
@@ -0,0 +1,15 @@
+package coding;
+
+public class Day36 {
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            if (i % 3 == 0 && i % 5 == 0 ){
+            System.out.println(i);
+        
+            }
+        }
+        
+    }
+    
+}
