519cf5020ad88724806fcab51cfbd0173643e0cd
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-15 20:18:23 +0800

    Create Day38.java

---
+++ Day38.java
@@ -0,0 +1,11 @@
+package day38;
+
+public class Day38 {
+    
+    public static void main(String[] args) {
+        for(int i = 1; i <= 20; i += 2){
+            System.out.println( i +  "Assalsmu'alaikum");
+        }
+    }
+    
+}
