f918b797645198b08a13cc84eb18da2d7141117e
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-16 21:27:31 +0800

    Create Day39.java

---
+++ Day39.java
@@ -0,0 +1,15 @@
+package ganjilGenap;
+
+public class ganjilGenap {
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            if(i%2 == 0){
+                System.out.println(i+" adalah angka genap");
+            }else{
+                System.out.println(i+" adalah angka ganjil ");
+        
+        
+    }
+    
+        }
