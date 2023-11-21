820b68277218ae50f447ccdc36e256001aa2eb96
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-21 22:01:30 +0800

    Create Day44.java

---
+++ Day44.java
@@ -0,0 +1,16 @@
+package Day44;
+
+public class Day44 {
+    
+    public static void main(String[] args) {
+        int i;
+        for (i = 2; i <= 10; i++) { 
+        if (i == 5) { 
+        continue; 
+      }
+      System.out.println(i + " + " + i + " = "+ (i+i));
+    }
+        
+    }
+    
+}
