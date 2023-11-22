bb62d8a266e221de033b5407a4e11f2b057c258e
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-22 18:42:52 +0800

    Create Day45.java

---
+++ Day45.java
@@ -0,0 +1,19 @@
+package Day45;
+
+public class Day45 {
+    
+    public static void main(String[] args) {
+        int i = 2;
+        while (i < 15) {
+          if (i == 4) {
+              i++;
+         continue;
+      }
+      System.out.println("Angka saat ini : " + i);
+      i++;
+        
+    }
+    }
+
+}
+
