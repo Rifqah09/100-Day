f5e4766ee585092bab7d5c57490dc37bcc6007cd
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-24 21:14:36 +0800

    Create Day47.java

---
+++ Day47.java
@@ -0,0 +1,25 @@
+package day47;
+
+public class Day47 {
+    
+    public static void main(String[] args) {
+        int[] angka = new int[50];
+
+	    	   int x = 1;
+        for (int i = 0; i < angka.length; i++) {
+		           	angka[i] = x;
+            x++;
+        }
+
+		       int total = 0;
+        for (int i = 0; i < angka.length; i++) {
+            total+=angka[i];
+		        }
+
+	        	System.out.println(total);
+    
+    }
+}
+        
+    
+        
