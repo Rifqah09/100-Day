3dca63aa873bdebe5b4407d026b74ff218607af7
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-26 20:42:07 +0800

    Create Day49.java

---
+++ Day49.java
@@ -0,0 +1,14 @@
+package Day49;
+
+public class Day49 {
+    
+    public static void main(String[] args) {
+        double[] nilai = {75.0,80.0,85.0,90.0,95.0};
+        System.out.println("Nilai Ujian DDP : " + nilai[1]);
+        System.out.println("Semua Nilai : ");
+        for (int i = 0; 1 < nilai.length; i++){
+            
+        }
+            System.out.println(nilai[1] + " ");
+          }
+}
