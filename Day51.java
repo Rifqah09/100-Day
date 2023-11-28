255d908847b84e005f9a4ba93ff05a89a172ca13
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-11-28 21:54:46 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,11 @@
+package Day51;
+
+public class Day5q {
+    
+    public static void main(String[] args) {
+        int[] nilai = {70,75,80,85,90,95};
+        int jumlah = nilai[0]+nilai[1]+nilai[2]+nilai[3]+nilai[4]+nilai[5];
+        System.out.println("Total nilai : " + jumlah);
+    }
+    
+}
