802329a7ee4a7b8f690d26141f020f64d7a2a947
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-12-04 20:59:14 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,18 @@
+package Day57;
+
+import java.util.Arrays;
+
+public class day57 {
+    
+    public static void main(String[] args) {
+        System.out.println("~array menggunakan for-each~");
+        int[] nilai = {65,70,75,80,85,90,95};
+        int i = 0;
+        do{
+            System.out.println(nilai[i]);
+            i++;
+        }while(i<nilai.length);
+        
+    
+    }
+}
