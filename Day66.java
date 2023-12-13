1467a7190d188bd2782fac033742c6f2fb70544f
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-12-13 20:07:04 +0800

    Update Day66.java

--- Day66.java
+++ Day66.java
@@ -4,19 +4,19 @@ public class Day66 {
     
     public static void main(String[] args) {
         
-        int[] array = { 1, 2, -3, 4, -5, 6, -7, -8, -9, 10 };
+        int[] angka = { 1, 2, -3, 4, -5, 6, -7, -8, -9, 10 };
 
         System.out.println("Bilangan positif:");
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > 0) {
-                System.out.println(array[i]);
+        for (int i = 0; i < angka.length; i++) {
+            if (angka[i] > 0) {
+                System.out.println(angka[i]);
             }
         }
 
         System.out.println("Bilangan negatif:");
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
-                System.out.println(array[i]);
+        for (int i = 0; i < angka.length; i++) {
+            if (angka[i] < 0) {
+                System.out.println(angka[i]);
             }
   
     }
