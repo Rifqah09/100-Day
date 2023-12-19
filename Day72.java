5d73b9710652dd0a4820456cca98707e00011d6c
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2023-12-19 19:51:18 +0800

    Create Day72.java

---
+++ Day72.java
@@ -0,0 +1,37 @@
+package Day72;
+
+public class Day72 {
+    
+    public static void main(String[] args) {
+        
+        int[][] angka = {
+            {1, -2, 3},
+            {-4, 5, -6},
+            {7, -8, 9}
+        };
+
+        
+        System.out.println("Bilangan Positif dalam Angka :");
+
+        
+        for (int i = 0; i < angka.length; i++) {
+            for (int j = 0; j < angka[i].length; j++) {
+                if (angka[i][j] > 0) {
+                    System.out.print(angka[i][j] + " ");
+                }
+            }
+        }
+    
+
+
+
+
+            }
+        
+    
+
+
+
+
+    }
+
