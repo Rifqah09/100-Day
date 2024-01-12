b35e6b0aebdc208846373cce6821327c71d8d878
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2024-01-13 07:20:20 +0800

    Create Day97.java

---
+++ Day97.java
@@ -0,0 +1,16 @@
+public class Day97 {
+    
+    public static void main(String[] args) {
+        int angka = 24678;
+
+        while (angka > 0) {
+            int digit = angka % 10;
+            System.out.println(digit);
+
+            angka = angka / 10
+        }
+        
+
+    }
+    
+}
